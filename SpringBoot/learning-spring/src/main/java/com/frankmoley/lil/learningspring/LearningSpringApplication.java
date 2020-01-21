abd1c9b1d684d6329b3fd0d3856072d405806819
d846590 <Sunshine.Cotejo@team.telstra.com>
2020-01-21 16:06:11 +0800

    Updated

--- SpringBoot/learning-spring/src/main/java/com/frankmoley/lil/learningspring/LearningSpringApplication.java
+++ SpringBoot/learning-spring/src/main/java/com/frankmoley/lil/learningspring/LearningSpringApplication.java
@@ -1,7 +1,17 @@
 package com.frankmoley.lil.learningspring;
 
+import com.frankmoley.lil.learningspring.data.entity.Guest;
+import com.frankmoley.lil.learningspring.data.entity.Reservation;
+import com.frankmoley.lil.learningspring.data.entity.Room;
+import com.frankmoley.lil.learningspring.data.repository.GuestRepository;
+import com.frankmoley.lil.learningspring.data.repository.ReservationRepository;
+import com.frankmoley.lil.learningspring.data.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LearningSpringApplication {
@@ -23,6 +33,24 @@ public class LearningSpringApplication {
 
 	}
 
+	@RestController
+	@RequestMapping("/guests")
+	public class GuestController{
+		@Autowired
+		private GuestRepository guestRepository;
 
+		@GetMapping
+		public Iterable<Guest> getGuests(){return this.guestRepository.findAll();}
+	}
+
+	@RestController
+	@RequestMapping("/reservations")
+	public class ReservationController{
+		@Autowired
+		private ReservationRepository reservationRepository;
+
+		@GetMapping
+		public Iterable<Reservation> getReservations(){return this.reservationRepository.findAll();}
+	}
 
 }
